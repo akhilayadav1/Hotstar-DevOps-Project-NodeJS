a00551ca36e143a4660eaf7dcf3632c7763b7dcc
akhilayadav1 <159409016+akhilayadav1@users.noreply.github.com>
2024-09-30 12:39:14 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mys3bucket-hotstar4" # Replace with your actual S3 bucket name
+    bucket = "akhila-bucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
